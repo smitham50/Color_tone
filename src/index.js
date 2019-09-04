e6f01ee00cffbab8976ac8b7a955b5ba7144a3e2
Aaron Smith <smitham50@gmail.com>
2019-09-04 16:34:21 -0400

    made randomized arpeggiator

--- src/index.js
+++ src/index.js
@@ -5,20 +5,29 @@ const button = document.querySelector('button')
 
 button.addEventListener('click', () => setup())
 
+const notes = ['a', 'c', 'e']
+const octaves = ['2', '3', '4', '5', '1']
+
+
 function setup() {
 
-  console.log("Yo")
-
-  bassSynth = new Tone.MembraneSynth().toMaster()
+  bassSynth = new Tone.Synth().toMaster()
 
   loopBeat = new Tone.Loop(song, "4n")
  
-  Tone.Transport.bpm.value = 100
+  Tone.Transport.bpm.value = 160
   Tone.Transport.start()
   loopBeat.start(0)
 }
 
 function song(time) {
-  bassSynth.triggerAttackRelease('c1', '8n', time)
-  console.log(time)
+  bassSynth.triggerAttackRelease(randomNote(notes, octaves), '8n', time)
+  console.log(randomNote(notes, octaves))
+}
+
+function randomNote(notesArray, octaveArray) {
+  let noteIndex = Math.floor(Math.random() * 3)
+  let octaveIndex = Math.floor(Math.random() * 5)
+
+  return `${notesArray[noteIndex]}${octaveArray[octaveIndex]}`
 }
