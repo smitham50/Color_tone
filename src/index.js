26d910e5d7960a419e4eb5997291338efd647101
Aaron Smith <smitham50@gmail.com>
2019-09-05 08:32:20 -0400

    arpeggiation more varied

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
 let loopBeat
-let bassSynth
+let synth
+let filter
 
 const button = document.querySelector('button')
 
 button.addEventListener('click', () => setup())
 
-const notes = ['a', 'c', 'e', 'd']
-const octaves = ['1', '2', '3', '4']
+const notes = ['a', 'c', 'e', 'd', 'f']
+const octaves = ['1', '2', '3', '4', '5']
 const durations = ['2', '4', '8', '16']
 
 
 function setup() {
-  bassSynth = new Tone.Synth().toMaster()
-
+  synth = new Tone.Synth().toMaster()
+  // filter = new Tone.Filter({
+  //   type: 'lowpass'
+  // })
   loopBeat = new Tone.Loop(song, "4n")
  
-  Tone.Transport.bpm.value = 160
+  Tone.Transport.bpm.value = 300
   Tone.Transport.start()
   loopBeat.start(0)
 }
 
 function song(time) {
-  bassSynth.triggerAttackRelease(randomNote(notes, octaves), randomDuration(durations), time)
-  console.log(randomNote(notes, octaves))
+  synth.triggerAttackRelease(randomNote(notes, octaves), randomDuration(durations), time)
+  console.log(randomNote(notes, octaves), randomDuration(durations))
 }
 
 function randomNote(notesArray, octaveArray) {
-  let noteIndex = Math.floor(Math.random() * 4)
-  let octaveIndex = Math.floor(Math.random() * 4)
+  let noteIndex = Math.floor(Math.random() * 5)
+  let octaveIndex = Math.floor(Math.random() * 5)
 
   return `${notesArray[noteIndex]}${octaveArray[octaveIndex]}`
 }
