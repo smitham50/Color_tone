475fc26febf4d7179163fc465a43190b9ed48c1e
Aaron Smith <smitham50@gmail.com>
2019-09-13 12:29:24 -0400

    added voice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 let loopBeat
 let synth
+let mono
 
 const body = document.querySelector('body')
 const button = document.querySelector('button')
@@ -9,20 +10,44 @@ button.addEventListener('click', () => setup())
 const notes = ['ab', 'g', 'eb', 'db', 'bb', 'c']
 const octaves = ['1', '2', '3', '4', '5']
 const durations = ['2', '4', '8', '16']
+const monoNotes = ['ab', 'bb', 'eb']
 
 
 function setup() {
-  synth = new Tone.Synth().toMaster()
+  synth = new Tone.Synth({
+    "oscillator": {
+      "type": "sine"
+    }
+  }).toMaster()
+
+  mono = new Tone.MonoSynth({
+    "oscillator": {
+      "type": "triangle"
+    },
+    "filter": {
+      "frequency": 300,
+      "type": "lowpass",
+      "rolloff": -24
+    },
+    "envelope": {
+      "attack": 0.1,
+      "decay": .05
+    }
+  }).toMaster()
+
   loopBeat = new Tone.Loop(song, "4n")
  
-  Tone.Transport.bpm.value = 300
+  Tone.Transport.bpm.value = 280
   Tone.Transport.start()
   loopBeat.start(0)
 }
 
+function triggerMono(note) {
+  mono.triggerAttackRelease(note, "16n")
+}
+
 function song(time) {
   synth.triggerAttackRelease(randomNote(notes, octaves), randomDuration(durations), time)
-  console.log(randomNote(notes, octaves), randomDuration(durations))
 }
 
 function randomNote(notesArray, octaveArray) {
@@ -32,6 +57,7 @@ function randomNote(notesArray, octaveArray) {
   if (noteIndex === 0) {
     body.style.background = 'red'
     button.style.transform = "scale(1.2)"
+    triggerMono(`${monoNotes[2]}${octaveArray[octaveIndex]}`)
   } else if (noteIndex === 1) {
     body.style.background = 'yellow'
     button.style.transform = "scale(1.4)"
@@ -44,6 +70,7 @@ function randomNote(notesArray, octaveArray) {
     button.style.transform = "scale(1.6)"
   } else if (noteIndex === 5) {
     body.style.background = 'pink'
+    triggerMono(`${monoNotes[0]}${octaveArray[octaveIndex]}`)
   }
 
   return `${notesArray[noteIndex]}${octaveArray[octaveIndex]}`
